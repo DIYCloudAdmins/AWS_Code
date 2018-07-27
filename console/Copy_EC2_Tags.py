e62cdf4ef3aa92851432eefc8a46f53f44b6e1ed
PaulBeauvais <Paul@diyautomate.me>
2018-07-27 14:48:06 -0400

    Added loging function.

--- console/Copy_EC2_Tags.py
+++ console/Copy_EC2_Tags.py
@@ -1,9 +1,30 @@
 import boto3
+from datetime import datetime
 #stolen and modified from: https://gist.github.com/mlapida/931c03cce1e9e43f147b
 is_test = True
+outPutToText = True
+if outPutToText == True:
+    logFile = open("Copy_EC2_Tags_Log" + str(datetime.now()).replace(" ", "").replace(":",".") + ".txt","w")
+outPutToPrint = False
 instances = boto3.resource('ec2').instances.all()
+instanceID = ""
 
 copyable_tag_keys = ["Name", "Application", "Project", "Component", "Environment", "Owner"]
+
+
+def output(tagString):
+    global logFile
+    global instanceID
+    global outPutToPrint
+    global outPutToText
+
+    for labels in [" ", "\'", "{", "}", "[", "]"]:
+        tagDictionary = tagDictionary.replace(labels,"")
+    if outPutToPrint = True:
+        print(tagDictionary)
+    if outPutToText = True:  
+        logFile.write(tagDictionary)
+        logFile.write("\n")
 
 for instance in instances:
     copyable_tags = [tags for tags in instance.tags if tags["Key"] in copyable_tag_keys] if instance.tags else []
@@ -11,14 +32,16 @@ for instance in instances:
         continue
 
     # Tag the EBS Volumes
-    print(f"{instance.instance_id}: {instance.tags}")
+    #print(f"{instance.instance_id}: {instance.tags}")
     for vol in instance.volumes.all():
-        print(f"{vol.attachments[0]['Device']}: {copyable_tags}")
+        output(instance.id)
+        output(f"{vol.attachments[0]['Device']}: {copyable_tags}")
         if not is_test:
             vol.create_tags(Tags=copyable_tags)
 
     # Tag the Elastic Network Interfaces
     for eni in instance.network_interfaces:
-        print(f"eth{str(eni.attachment['DeviceIndex'])}: {copyable_tags}")
+        output(instance.id)
+        output(f"eth{str(eni.attachment['DeviceIndex'])}: {copyable_tags}")
         if not is_test:
             eni.create_tags(Tags=copyable_tags)
