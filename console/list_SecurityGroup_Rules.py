9f498f7e565fed8835cd6376cf5bbf8166ba0372
Beauvais <Paul.Beauvais@hibu.com>
2019-11-14 16:30:55 -0500

    write from scratch

--- console/list_SecurityGroup_Rules.py
+++ console/list_SecurityGroup_Rules.py
@@ -1,20 +1,52 @@
 import boto3
+import pandas as pd
+
 ec2 = boto3.client('ec2')
 response = ec2.describe_security_groups()
-for securityGroups in response['SecurityGroups']:
-    print("Security Group Name: "+securityGroups['GroupName'])
-    print("Egress Rules: ")
-    for egressRules in securityGroups['IpPermissionsEgress']:
-        print("IP Protocol: "+egressRules['IpProtocol'])
-        for ipRange in egressRules['IpRanges']:
-            print("IP Ranges: "+ipRange['CidrIp'])
-    print("Ingress Rules: ")
-    for Permissions in securityGroups['IpPermissions']:
-        print("IP Protocol: "+Permissions['IpProtocol'])
-        try:
-            print("PORT: "+str(Permissions['FromPort']))
-            for ipRange in Permissions['IpRanges']:
-                print("IP Ranges: "+ipRange['CidrIp'])
-        except Exception:
-            print("No Port")
-            continue
+
+securityGroups = []
+
+for securityGroup in response['SecurityGroups']:
+    sg = {}
+    sg['name'] = securityGroup['GroupName']
+    sg['vpcId'] = securityGroup.get('vpc_id', None)
+    sg['groupId'] = securityGroup['GroupId']
+    sg['accountId'] = securityGroup['OwnerId']
+    #outbound rules
+    egressRules = []
+    if len(securityGroup['IpPermissionsEgress']) != 0:
+        for egressRule in securityGroup['IpPermissionsEgress']:
+            ipRule = {}
+            ipRule['egressIpProtocol'] = egressRule['IpProtocol']
+            ipranges = []
+            for ipRange in egressRule['IpRanges']:
+                ipranges.append(ipRange['CidrIp'])
+            ipRule['egressIpRange'] = ipRange
+            ipRule['egressDescription'] = egressRule.get('Description', '')
+            egressRules.append(ipRule)
+    sg['egressRules'] = egressRules
+    #inbound rules
+    ingressRules = []
+    if len(securityGroup['IpPermissions']) != 0:
+        for Permission in securityGroup['IpPermissions']:
+            ingressRule = {}
+            
+            ingressRule['inboundProtocol'] = Permission['IpProtocol']
+
+            if 'FromPort' in Permission:
+                ingressRule['fromPort'] = Permission['FromPort']
+                ingressRule['toPort'] = Permission['ToPort']
+            else:
+                ingressRule['fromPort'] = ''
+                ingressRule['toPort'] =''               
+    
+            ingressRule['ipProtocol'] = Permission['IpProtocol']
+            ingressRule['IpRanges'] = Permission['IpRanges']
+            ingressRule['userIdGroupPairs'] = Permission['UserIdGroupPairs']
+            ingressRules.append(ingressRule)
+    sg['ingressRules'] = ingressRules
+
+    securityGroups.append(sg)
+
+securityGroupDF = pd.DataFrame(securityGroups)
+print(securityGroupDF.head())
